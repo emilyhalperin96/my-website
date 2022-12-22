45f39f0be28298bef086817ebe6f1f9384a7395e
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP.lan>
2022-12-22 15:00:53 -0500

    commit tree e9a8375a68e39d2d8fc4af8e04c5b550c78386fd

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+  alert('I was clicked!');
+});
